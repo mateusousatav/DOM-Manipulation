4b5bcc34569fbc509aab191b4c6a7a661ca07982
mateusousatav <mateusstavares2008@gmail.com>
2025-10-30 09:13:53 -0400

    fixed bug

--- script2.js
+++ script2.js
@@ -1,37 +1,8 @@
 document.getElementById("changeP").innerHTML = "1";
-document.getElementById("addButton").innerHTML = "add value";
+document.getElementById("addButton").innerHTML = "add by 1";
 
-document.getElementById("MultiplicationTable").addEventListener("click", function() {
-    let origin = document.getElementById(changeP).innerHTML;
-    let arr = [];
-    for(let i = 0; i < 5; i++) {
-        arr.push(parseInt(origin) * (i + 1));
-    }
-
-    document.add
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-document.getElementByID("addButton").addEventListener("click", function() {
-    let origin = document.getElementById("changeP").innerHTML;
-    console.log(origin);
-    //casting to convert string to number
-    origin = parseInt(origin) + 1;
-    document.getElementById("changeP").innerHTML = origin;
-})
+document.getElementById("addButton").addEventListener("click",function(){
+    let number = parseInt(document.getElementById("changeP").innerHTML) ;
+    number=number + 1;
+    document.getElementById("changeP").innerHTML=number;
+});
