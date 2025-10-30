a4cc0a0e2f943d5060d7b8511d99a88b6f6bac17
mateusousatav <mateusstavares2008@gmail.com>
2025-10-30 17:44:07 -0400

    Add event listeners for button actions and binary conversion

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-document.getElementById("taskname1").innerHTML = "Complete the project documentation";
-
-let task = document.getElementById("compl1").addEventListener("click", function() {
-    document.getElementById("taskname1").style.color = "red";
-    document.getElementById("taskname2").style.textDecoration = "line-through";
-})
-
-document.getElementById("addTask").addEventListener("click", function () {
-    event.preventDefault(); 
-    let newTask = document.getElementById("new-task").value; // Fixed typo
-    console.log(newTask);
-    document.getElementById("taskname3").innerHTML = newTask;
+document.getElementById("add_button").addEventListener("click",function(){
+    let number = parseInt(document.getElementById("changeP").innerHTML) ;
+    number=number + 1;
+    document.getElementById("changeP").innerHTML=number;
 });
+
+document.getElementById("multiply_button").addEventListener("click", function() {
+    let number = parseInt(document.getElementById("changeP").innerHTML);
+    for(let i = 0; i < 10; i++) {
+        let mult_factor = parseInt(document.getElementsByClassName("mult-factors")[i].innerHTML);
+        let multiple = parseInt(document.getElementsByClassName("multiples")[i].innerHTML);
+        multiple = mult_factor * number;
+        document.getElementsByClassName("multiples")[i].innerHTML = multiple;
+    }
+});
+
+function decimalToBinaryManual(num) {
+  if (num === 0) return "0";
+
+  let binary = "";
+  while (num > 0) {
+    let remainder = num % 2;
+    binary = remainder + binary;
+    num = Math.floor(num / 2);
+  }
+  return binary;
+}
+
+document.getElementById("binary_button").addEventListener("click", function() {
+    let decimalValue = parseInt(document.getElementById("changeP").innerHTML);
+    let binaryValue = decimalToBinaryManual(decimalValue);
+    document.getElementById("binary").innerHTML = binaryValue;
+    
+});
+
+
+
