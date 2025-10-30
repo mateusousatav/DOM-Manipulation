9be6f34f530a6168beb97f319759582c6d74ef9f
mateusousatav <mateusstavares2008@gmail.com>
2025-10-30 09:49:25 -0400

    fixed mult table

--- script2.js
+++ script2.js
@@ -1,8 +1,19 @@
 document.getElementById("changeP").innerHTML = "1";
-document.getElementById("addButton").innerHTML = "add by 1";
+document.getElementById("add_button").innerHTML = "add by 1";
 
-document.getElementById("addButton").addEventListener("click",function(){
+document.getElementById("add_button").addEventListener("click",function(){
     let number = parseInt(document.getElementById("changeP").innerHTML) ;
     number=number + 1;
     document.getElementById("changeP").innerHTML=number;
 });
+
+document.getElementById("multiply_button").addEventListener("click", function() {
+    let number = parseInt(document.getElementById("changeP").innerHTML);
+    for(let i = 0; i < 10; i++) {
+        let mult_factor = parseInt(document.getElementsByClassName("mult-factors")[i].innerHTML);
+        let multiple = parseInt(document.getElementsByClassName("multiples")[i].innerHTML);
+        multiple = mult_factor * number;
+        document.getElementsByClassName("multiples")[i].innerHTML = multiple;
+    }
+});
+
