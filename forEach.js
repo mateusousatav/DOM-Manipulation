31b20ee35ceb117c6899e54f79490ad54488c6b7
mateusousatav <mateusstavares2008@gmail.com>
2025-11-05 09:01:31 -0500

    a

---
+++ forEach.js
@@ -0,0 +1,8 @@
+const prices = [5, 10, 20];
+const updatedPrices = prices.map(price => price + 2);
+console.log(updatedPrices);
+
+const arr1 = ["a", "b", "c", "d", "e"];
+const uppercase = arr1.map(element => element.toUpperCase());
+console.log(uppercase);
+
